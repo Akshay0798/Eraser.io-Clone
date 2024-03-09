4abdbe5d7b7166385e4cea85e269f8b92d5f849f
Akshay Umredkar <94985332+Akshay0798@users.noreply.github.com>
2024-03-09 13:58:54 +0530

    initial commit

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,12 +1,38 @@
 "use client"
 import { Button } from '@/components/ui/button'
-import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs'
-import React from 'react'
+import { api } from '@/convex/_generated/api'
+import { LogoutLink, useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import { useConvex, useMutation, useQuery } from 'convex/react'
+import { Result } from 'postcss'
+import React, { useEffect } from 'react'
 
 const Dashboard = () => {
+  const convex = useConvex()
+  const {user}:any = useKindeBrowserClient();
+  // const getUser = useQuery(api.user.getUser,{email:user?.email});
+  const createUser = useMutation(api.user.createUser);
+  
+useEffect(()=>{
+    if(user){
+      checkUser()
+    }
+  },[user])
+
+  const checkUser = async()=>{
+    const result = await convex.query(api.user.getUser,{email:user?.email});
+    if(!result?.length){
+      createUser({
+        name:user.given_name,
+        email:user.email,
+        image:user.picture
+      }).then(resp=>{
+        console.log(resp)
+      })
+    }
+  }
   return (
     <div>
-      Dashboard
+      
       <Button>
         <LogoutLink>Logout</LogoutLink>
       </Button>
---
+++ convex/user.tsx
@@ -0,0 +1,28 @@
+import {v} from 'convex/values'
+import { mutation, query } from './_generated/server'
+
+export const getUser=query({ // Exporting a GraphQL query named 'getUser'.
+    args:{
+        email:v.string() // Expecting an 'email' argument, validated as a string using 'v.string()'.
+    },
+
+    handler: async (ctx, args) => { // Defining the function that will be executed when the query is invoked. It's an asynchronous function that takes 'ctx' (context) and 'args' (arguments) as parameters.
+        const result = await ctx.db.query('user') // Querying the database to get all users.
+            .filter((q) => q.eq(q.field('email'), args.email)) // Filtering the result to find the user whose email matches the provided email.
+            .collect(); // Collecting and returning the filtered result.
+
+
+    return result;
+    },
+})
+
+export const createUser=mutation({ 
+    args:{
+        name:v.string(),
+        email:v.string(),
+        image:v.string()
+    },
+    handler: async (ctx, args) => { // Defining the function that will be executed when the mutation is invoked. It's an asynchronous function that takes 'ctx' (context) and 'args' (arguments) as parameters.
+        return await ctx.db.insert("user", args); // Inserting a new user into the database using 'ctx.db.insert("user", args)' and returning the result.
+    },
+})
